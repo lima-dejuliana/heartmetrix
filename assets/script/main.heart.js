097418f8e8573a4c6278e5229c679daf47d5cf66
Juliana Lima <juliana.lima@RAI.local>
2023-08-04 18:59:09 -0300

    rel geral

--- assets/script/main.heart.js
+++ assets/script/main.heart.js
@@ -20,4 +20,89 @@ $(document).ready(function () {
     $('.areah__nav').toggleClass('off', 300, 'easeOutSine');
     $('main').toggleClass('off', 300, 'easeOutSine');
   });
+
+  $('.tb').each(function (index, table) {
+    let dataTb = $(this).attr('data-table');
+    if (dataTb == 'tb-smp') {
+      let table = new DataTable($(this), {
+        info: false,
+        ordering: false,
+        paging: false,
+        searching: false,
+      });
+    } else if (dataTb == 'tb-pag') {
+      let table = new DataTable($(this), {
+        info: false,
+        ordering: false,
+        searching: false,
+        lengthChange: false,
+        scrollX: true,
+        pagingType: 'simple_numbers',
+        pageLength: 5,
+        language: {
+          paginate: {
+            previous:
+              '<img src="./assets/images/icons/icon-arrow-circle-left.svg" alt="Anterior">',
+            next: '<img src="./assets/images/icons/icon-arrow-circle-right.svg" alt="Próximo">',
+          },
+        },
+      });
+    }
+  });
+
+  $('.myChart').each(function (index, chart) {
+    let ctx = $(this);
+
+    new Chart(ctx, {
+      type: 'doughnut',
+      data: {
+        labels: ['60+', '20 - 40', '40 - 60'],
+        datasets: [
+          {
+            label: '# of Votes',
+            data: [15, 40, 45],
+            borderWidth: 1,
+            backgroundColor: [
+              'rgba(96, 195, 173, 1)',
+              'rgba(53, 140, 203, 1)',
+              'rgba(79, 198, 224, 1)',
+              'rgba(53, 203, 83, 1)',
+              'rgba(198, 203, 53, 1)',
+              'rgba(164, 53, 203, 1)',
+            ],
+          },
+        ],
+      },
+
+      options: {
+        plugins: {
+          legend: {
+            position: 'right',
+            // align: 'end',
+            labels: {
+              usePointStyle: true,
+              font: {
+                size: 14, // Altere esse valor para o tamanho da fonte desejado
+              },
+            },
+          },
+        },
+        cutout: 70,
+        scales: {
+          x: {
+            display: false,
+            grid: {
+              display: false,
+            },
+          },
+          y: {
+            display: false,
+            grid: {
+              display: false,
+            },
+          },
+        },
+      },
+    });
+  });
 });
